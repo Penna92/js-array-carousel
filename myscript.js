2f23ba21be2d5fa6e4265c9104c77a250afb5a93
Penna92 <giacomopenna92@gmail.com>
2022-03-30 17:03:45 +0200

    Commentato elementi html e creati elementi dinamicamente (non completato)

--- myscript.js
+++ myscript.js
@@ -1,5 +1,3 @@
-// MILESTONE 1
-// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande a sinistra e le thumbnails sulla destra in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
 // MILESTONE 2
 // Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
 // MILESTONE 3
@@ -15,3 +13,58 @@
 // 1. costruiamo del carosello una versione statica contenente un'immagine grande con del testo ben posizionato e una miniatura. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
 // 2. scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
 // 3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+for(let i=0; i<items.length; i++){
+    let divItem = document.createElement("div");
+    document.getElementById("left-container").prepend(divItem);
+    divItem.classList.add("item");
+    let img = document.createElement("img");
+    img.setAttribute("src", items[i]);
+    let itemText = document.createElement("div");
+    itemText.setAttribute("class", "item-container-left-text")
+    let titleText = document.createElement("div");
+    titleText.setAttribute("class", "title-text");
+    let country = document.createElement("h4");
+    country.append(title[i]);
+    let subtitleText = document.createElement("div");
+    subtitleText.setAttribute("class", "subtitle-text");
+    let subtitle = document.createElement("h6");
+    subtitleText.append(subtitle);
+    subtitle.append(text[i]);
+    itemText.append(titleText, subtitleText);
+    titleText.append(country);
+    divItem.append(img, itemText);
+}
+
+let currentIndex = 0;
+const listaImmagini = document.getElementsByClassName("item");
+const next = document.querySelector(".next");
+next.addEventListener("click", function () {
+  listaImmagini[currentIndex].classList.remove("active");
+  currentIndex += 1;
+  listaImmagini[currentIndex].classList.add("active");
+});
